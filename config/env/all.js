e3eee70ae86a6f09b3cdac53e887120cb2469896
Jisan Zaman <jzthegreat@gmail.com>
2014-05-29 15:32:57 -0400

    changed README

--- config/env/all.js
+++ config/env/all.js
@@ -2,13 +2,13 @@
 
 module.exports = {
 	app: {
-		title: 'MEAN.JS',
-		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-		keywords: 'mongodb, express, angularjs, node.js, mongoose, passport'
+		title: 'EventFlow',
+		description: '',
+		keywords: ''
 	},
 	port: process.env.PORT || 3000,
 	templateEngine: 'swig',
-	sessionSecret: 'MEAN',
+	sessionSecret: 'EVENTFLOW',
 	sessionCollection: 'sessions',
 	assets: {
 		lib: {
--- config/env/development.js
+++ config/env/development.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/mean-dev',
+	db: 'mongodb://localhost/eventflow-dev',
 	app: {
-		title: 'MEAN.JS - Development Environment'
+		title: 'EventFlow'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
